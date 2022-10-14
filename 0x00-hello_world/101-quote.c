2bcb55a78a0bdfaa9ae2a93409f2f68588729a24
bsltan <califchichi@yahoo.com>
2022-10-14 16:33:57 -0700

    C - Hello, World

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,22 +5,16 @@
 
 
 /**
- *
- *  * main - prints exactly "and that piece of art is useful"
- *
- *   * - Dora Korpar, 2015-10-19",
- *
- *    * followed by a new line, to the standard error.
- *
- *     * Return: Always 0 (Success)
- *
- *      */
+ *   main - Entry
+ *   Return - 1
+ */
 
 int main(void)
 
 {
 
-		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+		write(1, "and that piece of art is useful\"
+	- Dora Korpar, 2015-10-19\n", 59);
 
 			return (1);
 
