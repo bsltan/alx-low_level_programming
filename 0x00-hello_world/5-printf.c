29c22b6599dc264d98c2fa5de57e0361da34d1bb
bsltan <califchichi@yahoo.com>
2022-10-14 15:47:54 -0700

    C - Hello, World

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,16 +1,9 @@
 #include <stdio.h>
-
-
-
 /**
+ * main - entry
  *
- *  * main - prints the phrase with proper grammar,
- *
- *   * but the outcome is a piece of art,
- *
- *    * Return: 0 if exited properly, non-zero otherwise
- *
- *     */
+ * Return: Always 0
+ */
 
 int main(void)
 
