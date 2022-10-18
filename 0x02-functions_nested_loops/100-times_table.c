723d5ff55466573d5006cc4fdd60835c40c45ca1
bsltan <califchichi@yahoo.com>
2022-10-18 06:25:13 -0700

    C - Functions, nested loops

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -26,14 +26,6 @@ void print_times_table(int n)
 				}
 				else if (z > 9)
 				{
-					_putchar(',');
-					_putchar(32);
-					_putchar(32);
-					_putchar(((z / 10) % 10) + '0');
-					_putchar((z % 10) + '0');
-				}
-				else
-				{
 					if (y != 0)
 					{
 						_putchar(',');
@@ -43,8 +35,9 @@ void print_times_table(int n)
 					}
 					_putchar(z + '0');
 				}
-				_putchar('\n');
 			}
+			_putchar('\n');
 		}
 	}
 }
+
