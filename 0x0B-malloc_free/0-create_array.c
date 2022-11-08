bf116c46f4772a37bef2a698b8b6b009e79987ac
bsltan <califchichi@yahoo.com>
2022-11-08 02:07:11 -0800

    C - malloc, free

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,22 +12,22 @@ char *create_array(unsigned int size, char c)
 {
 	char *p;
 	unsigned int i = 0;
-	
+
 	if (size == 0)
 		return (NULL);
-	
+
 	p = (char *) malloc(sizeof(char) * size);
-	
+
 	if (p == NULL)
 		return (0);
-	
+
 	while (i < size)
 	{
 		*(p + i) = c;
 		i++;
 	}
-	
+
 	*(p + i) = '\0';
-	
+
 	return (p);
 }
